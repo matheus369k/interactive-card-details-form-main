ef0d3b2a9793e59653972373738903992e1198ad
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2023-11-18 15:22:47 -0300

    fuction of verifivation form creater...

--- js/index.js
+++ js/index.js
@@ -75,46 +75,133 @@ function CreaterContainerConfirmation() {
     return
 }
 
-function VerificationForm() {
-    const ErroList = [
-        "Correct",
-        "Current",
-        "Correct",
-        "Current",
-        "Correct"
+const ErroList = [
+    "Corrected",
+    "Corrected",
+    "Corrected",
+    "Corrected",
+    "Corrected"
+]
 
-    ]
-    const inputValues = document.querySelectorAll("input")
+const daysofMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
+
+
+const inputValues = document.querySelectorAll("input")
+function VerificationForm() {
+
 
     for (let i = 0; i < inputValues.length; i++) {
 
-        if (inputValues[i].value === '') {
-            inputValues[i].style.border = "1px solid red"
+        if (inputValues[i].value.length < 1) {
             ErroList[i] = "Can't be blank"
-
-            const Errormsg = document.createElement("span")
-            const ErrorText = document.createTextNode(`${ErroList[i]}`)
-            Errormsg.appendChild(ErrorText)
-            Errormsg.setAttribute("class", "Container_ErrorMsg")
-            Errormsg.setAttribute("id", `input_${i}`)
-            inputValues[i].parentNode.appendChild(Errormsg)
         }
         else
         {
-            inputValues[i].parentNode.removeChild(document.getElementById(`input_${i}`))
-            inputValues[i].style.border = "1px solid  hsl(270, 3%, 87%)"
+            ErroList[i] = "Corrected"
         }
-
-        console.log(ErroList.indexOf("Incorrect"));
+        ErrMsg(i)
     }
 
-
-    console.log(ErroList.indexOf("Incorrect"));
-
-    if (ErroList.indexOf("Can't be blank") === -1) {
+    if (document.getElementById("form_container").querySelectorAll("span").length === 0) {
         return CreaterContainerConfirmation()
     }
 
+    return
+}
 
+inputValues.forEach((inputValue, index) => {
+    inputValue.addEventListener("input", () => {
+        VerificationNumberValue(inputValue, index)
+    })
+})
+
+function monthVerification() {
+    if (inputValues[2].value < 13 && inputValues[2].value.length === 2) 
+    {
+        ErroList[2] = "Corrected"        
+    }
+    else 
+    {
+        if (document.getElementById(`input_${2}`)) {
+            inputValues[2].parentNode.removeChild(document.getElementById(`input_${2}`))
+        }
+        ErroList[2] = "Wrong format, numbers only"
+    }
+    ErrMsg(2)
+    return
+}
+
+function DayVerification() {
+    
+    if (inputValues[3].value <= daysofMonth[(inputValues[2].value) - 1]) {
+
+        ErroList[3] = "Corrected"
+    }
+    else 
+    {
+        if (document.getElementById(`input_${3}`)) {
+            inputValues[3].parentNode.removeChild(document.getElementById(`input_${3}`))
+        }
+        ErroList[3] = "Wrong format, numbers only"
+    }
+    ErrMsg(3)
+    return
+}
+
+function VerificationNumberValue(inputValue, index) {
+    if (Number(inputValue.value) || inputValue === inputValues[0]) 
+    {
+
+        if(inputValue === inputValues[2] || inputValue === inputValues[3])
+        {
+
+            monthVerification()
+
+            DayVerification()
+            
+            return
+        }
+
+        ErroList[index] = "Corrected"
+
+    }
+    else 
+    {
+        if (document.getElementById(`input_${index}`)) {
+            inputValues[index].parentNode.removeChild(document.getElementById(`input_${index}`))
+        }
+        ErroList[index] = "Wrong format, numbers only"
+    }
+
+    ErrMsg(index)
+    return
+
+}
+
+
+
+function ErrMsg(i) {
+
+    if (document.getElementById(`input_${i}`) === null) 
+    {
+
+        const Errormsg = document.createElement("span")
+        const ErrorText = document.createTextNode(`${ErroList[i]}`)
+        Errormsg.appendChild(ErrorText)
+        Errormsg.setAttribute("class", `Container_ErrorMsg_${i}`)
+        Errormsg.setAttribute("id", `input_${i}`)
+        inputValues[i].parentNode.appendChild(Errormsg)
+
+        inputValues[i].style.border = "1px solid red"
+
+    }
+
+    if (document.getElementById(`input_${i}`) && ErroList[i] === "Corrected")
+    {
+        inputValues[i].parentNode.removeChild(document.getElementById(`input_${i}`))
+
+        inputValues[i].style.border = "1px solid  hsl(270, 3%, 87%)"
+    }
+    return
 }
 
