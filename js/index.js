869a3e698982da92e4c8e8eca6f62d205915e8a4
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2023-11-19 19:06:12 -0300

    Project finily...

--- js/index.js
+++ js/index.js
@@ -4,26 +4,37 @@ confirmbBtn.addEventListener("click", comfirmationCard, false)
 
 function comfirmationCard(e) {
 
+    // Evitar o reload da pagina
     e.preventDefault()
 
+    // Construir a div responsavel pelo load
     const divEffect = document.createElement("div")
-    divEffect.setAttribute("class", "container_divEffect_verification_dados")
-    const fotherDivEffect = document.getElementById("form_container")
-    fotherDivEffect.appendChild(divEffect)
-    
 
-    setTimeout(()=> {
+    divEffect.setAttribute("class", "container_divEffect_verification_dados")
+
+    const fotherDivEffect = document.getElementById("form_container")
+
+    fotherDivEffect.appendChild(divEffect)
+
+    setTimeout(() => {
+
         fotherDivEffect.removeChild(document.querySelector(".container_divEffect_verification_dados"))
 
+        // Validador da informações inseridas
         VerificationForm()
+
     }, 1000)
 
+
     return
+
 }
+
 
 
 function CreaterContainerConfirmation() {
 
+    // div que e o pai do container da mensagem de confirmação
     const form_container = document.querySelector("#form_container")
 
     form_container.classList.add("hider_off")
@@ -38,6 +49,7 @@ function CreaterContainerConfirmation() {
     const divContainerfother = document.querySelector(".container_confirmation")
 
 
+    // icone
     const img = document.createElement("img")
 
     img.setAttribute("class", "container_confirmation_img")
@@ -47,6 +59,7 @@ function CreaterContainerConfirmation() {
     divContainerfother.appendChild(img)
 
 
+    // texto em destaque
     const h2 = document.createElement("h2")
 
     h2.setAttribute("class", "container_confirmation_h2")
@@ -57,7 +70,7 @@ function CreaterContainerConfirmation() {
 
     divContainerfother.appendChild(h2)
 
-
+    // texto sem destaque 
     const p = document.createElement("p")
 
     p.setAttribute("class", "container_confirmation_p")
@@ -68,7 +81,7 @@ function CreaterContainerConfirmation() {
 
     divContainerfother.appendChild(p)
 
-
+    // butão de continuar
     const button = document.createElement("button")
 
     button.setAttribute("class", "container_Confirmation_btn")
@@ -83,79 +96,95 @@ function CreaterContainerConfirmation() {
 
 
     return
+
 }
 
 
-const ErroList = [
-    "Corrected",
-    "Corrected",
-    "Corrected",
-    "Corrected",
-    "Corrected"
-]
+// listas para ajudar a validar os dados e a guarda-los.
+const ErroList = [ "Corrected", "Corrected", "Corrected", "Corrected", "Corrected" ]
 
-const daysofMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+const daysofMonth = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
 
-const cardRegister = {
-    name: "",
-    cardnumber: "",
-    day: "",
-    month: "",
-    cvc: ""
-}
+const cardRegister = { name: "", cardnumber: "", day: "", month: "", cvc: "" }
+
 
 
 const inputValues = document.querySelectorAll("input")
 
 function VerificationForm() {
 
+    // Verificar se  os dados inseridos correspondem ao esperados
     verificationValueRecomend()
 
     if (document.getElementById("form_container").childNodes.length < 22) {
+
+        // construtora da mensagem de confirmação
         return CreaterContainerConfirmation()
+
     }
 
+
     return
+
 }
 
-
+// ativado ao inserir um valor nos inputs
 inputValues.forEach((inputValue, index) => {
 
     inputValue.addEventListener("click", () => {
 
         if (document.querySelector(".selectInput")) {
+
             document.querySelector(".selectInput").classList.remove("selectInput")
         }
 
         inputValue.classList.add("selectInput")
 
+
+
         return
+
     })
 
     inputValue.addEventListener("input", () => {
 
+        // indentificar o valor adicionado e adicionalo  ao seu lugas ao lado
         AddValueCard(inputValue, index)
 
+
+
         return
+
     })
 })
 
+
+
 function AddValueCard(inputValue, index) {
 
-    if (inputValue === inputValues[3]) {
+    
+    //Nome
+    if (inputValue === inputValues[0]) {
+
+        cardRegister["name"] = inputValues[0].value
+
+        document.getElementById("name").innerHTML = `${cardRegister["name"]}`
+
+    }//Dia
+    else if (inputValue === inputValues[3]) {
 
         cardRegister["day"] = inputValues[3].value
 
         document.getElementById("day").innerHTML = `${cardRegister["day"]}`
 
-    }
+    }//Mes
     else if (inputValue === inputValues[2]) {
 
         cardRegister["month"] = inputValues[2].value
 
         document.getElementById("month").innerHTML = `${cardRegister["month"]}`
 
-    }
+    }//Numero do cartão
     else if (inputValue === inputValues[1]) {
 
         cardRegister["cardnumber"] = inputValues[1].value
@@ -163,6 +192,7 @@ function AddValueCard(inputValue, index) {
         for (let i = 0; i < 16; i += 4) {
 
             if (i === 0) {
+
                 document.getElementById("cardNumber").innerHTML = `${cardRegister["cardnumber"].slice(i, i + 4)}`
 
             }
@@ -174,7 +204,7 @@ function AddValueCard(inputValue, index) {
             }
         }
 
-    }
+    }// CvC
     else if (inputValue === inputValues[4]) {
 
         cardRegister["cvc"] = inputValues[4].value
@@ -183,26 +213,24 @@ function AddValueCard(inputValue, index) {
 
     }
 
-    if (inputValue === inputValues[0]) {
-
-        cardRegister["name"] = inputValues[0].value
-
-        document.getElementById("name").innerHTML = `${cardRegister["name"]}`
-
-    }
+    return
 }
+
 
 function verificationValueRecomend() {
 
+    // Validação do Nome
     if (inputValues[0].value.length > 3) {
 
         ErroList[0] = "Corrected"
-    } else {
+    } 
+    else {
 
         ErroList[0] = "Wrong format, numbers only"
     }
 
-    if (inputValues[3].value.length === 2 && (inputValues[3].value <= daysofMonth[Number(inputValues[2].value) - 1] && inputValues[2].value >= 1 || inputValues[2].value < 1 && inputValues[3].value <= 31)) {
+    // Validação do Dia
+    if (inputValues[3].value.length === 2 && (inputValues[3].value <= daysofMonth[Number(inputValues[2].value) - 1] && inputValues[2].value >= 1 || inputValues[2].value < 1 && inputValues[3].value <= 31) && inputValues[3].value > 0) {
 
         ErroList[3] = "Corrected"
 
@@ -212,23 +240,25 @@ function verificationValueRecomend() {
         ErroList[3] = "Wrong format, numbers only"
     }
 
-    if (inputValues[2].value < 13 && inputValues[2].value.length === 2) {
+    // Validação do Mes
+    if (inputValues[2].value < 13 && inputValues[2].value.length === 2 && inputValues[2].value > 0) {
 
         ErroList[2] = "Corrected"
 
+        // Validação do Dia em relaçao ao Mes digitado
         if (inputValues[3].value > daysofMonth[Number(inputValues[2].value) - 1]) {
 
             ErroList[3] = "Wrong format, numbers only"
 
         }
-
     }
     else {
 
         ErroList[2] = "Wrong format, numbers only"
     }
 
-    if (inputValues[1].value.length === 16) {
+    // Validação do Numero do cartão
+    if (inputValues[1].value.length === 16 && inputValues[1].value > 0) {
 
         ErroList[1] = "Corrected"
 
@@ -238,7 +268,8 @@ function verificationValueRecomend() {
         ErroList[1] = "Wrong format, numbers only"
     }
 
-    if (inputValues[4].value.length === 3) {
+    // Validação do CvC
+    if (inputValues[4].value.length === 3 && inputValues[4].value > 0) {
 
         ErroList[4] = "Corrected"
     }
@@ -248,38 +279,47 @@ function verificationValueRecomend() {
     }
 
 
+    // Validação do Tipo de valor inserido se corresponde ao requerido.
     for (let i = 0; i < inputValues.length; i++) {
 
-
+        // Validação se esta em branco
         if (inputValues[i].value.length === 0) {
 
             ErroList[i] = "Can't be blank"
 
-        } else if (inputValues[i].value.match(/^[a-zA-ZÃ\s]+$/) === null && inputValues[i] === inputValues[0]) {
+        } // validação se so contem letras, para o ( Nome )
+        else if (inputValues[i].value.match(/^[a-zA-ZÃ\s]+$/) === null && inputValues[i] === inputValues[0]) {
 
             ErroList[i] = "Wrong format, numbers only"
-            
-        } else if (inputValues[i].value.match(/^[0-9]+$/) === null && inputValues[i] !== inputValues[0]) {
+
+        }  // validação se so contem numeros, para o ( Numero do cartão, Datas e CvC )
+        else if (inputValues[i].value.match(/^[0-9]+$/) === null && inputValues[i] !== inputValues[0]) {
 
             ErroList[i] = "Wrong format, numbers only"
 
         }
 
+        // removedor de mensagem de erro para atualizala dependendo dos dados inseridos posteriormente
         if (document.getElementById(`input_${i}`)) {
 
             inputValues[i].style.border = "1px solid #dedede"
 
             inputValues[i].parentNode.removeChild(document.getElementById(`input_${i}`))
         }
+
         ErrMsg(i)
 
     }
 
+
     return
 }
 
+
+
 function ErrMsg(i) {
 
+    // Criador da mensagem alertando do valor não correspodente
     if (document.getElementById(`input_${i}`) === null && ErroList[i] !== "Corrected") {
 
         const Errormsg = document.createElement("span")
@@ -297,7 +337,8 @@ function ErrMsg(i) {
 
         inputValues[i].style.border = "1px solid red"
 
-    }
+
+    }// removedor da mensagem apos dado ser inserido corretamente
     else if (document.getElementById(`input_${i}`) && ErroList[i] === "Corrected") {
 
         inputValues[i].style.border = "1px solid #dedede"
