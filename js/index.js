d51629eec6eb83ef5c508017548b3baf37358f3c
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2023-11-18 21:56:56 -0300

    verifivation finnily...

--- js/index.js
+++ js/index.js
@@ -12,9 +12,8 @@ function comfirmationCard(e) {
 }
 
 
-
-
 function CreaterContainerConfirmation() {
+
     const form_container = document.querySelector("#form_container")
 
     form_container.classList.add("hider_off")
@@ -72,8 +71,10 @@ function CreaterContainerConfirmation() {
 
     divContainerfother.appendChild(button)
 
+
     return
 }
+
 
 const ErroList = [
     "Corrected",
@@ -83,125 +84,217 @@ const ErroList = [
     "Corrected"
 ]
 
-const daysofMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
+const daysofMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+const cardRegister = {
+    name: "",
+    cardnumber: "",
+    day: "",
+    month: "",
+    cvc: ""
+}
 
 
 const inputValues = document.querySelectorAll("input")
+
 function VerificationForm() {
 
 
     for (let i = 0; i < inputValues.length; i++) {
 
+        verificationTypeValue(inputValues[i], i)
+
         if (inputValues[i].value.length < 1) {
+
+            if (document.getElementById(`input_${i}`)) {
+
+                inputValues[i].parentNode.removeChild(document.getElementById(`input_${i}`))
+
+            }
+
             ErroList[i] = "Can't be blank"
+
         }
-        else
-        {
-            ErroList[i] = "Corrected"
-        }
+
         ErrMsg(i)
     }
 
-    if (document.getElementById("form_container").querySelectorAll("span").length === 0) {
+    if (document.getElementById("form_container").childNodes.length < 22) {
         return CreaterContainerConfirmation()
     }
 
+
     return
 }
 
+
 inputValues.forEach((inputValue, index) => {
+
+    inputValue.addEventListener("click", () => {
+
+        if (document.querySelector(".selectInput")) {
+            document.querySelector(".selectInput").classList.remove("selectInput")
+        }
+
+        inputValue.classList.add("selectInput")
+
+        return
+    })
+
     inputValue.addEventListener("input", () => {
-        VerificationNumberValue(inputValue, index)
+
+        verificationTypeValue(inputValue, index)
+
+        return
     })
 })
 
-function monthVerification() {
-    if (inputValues[2].value < 13 && inputValues[2].value.length === 2) 
-    {
-        ErroList[2] = "Corrected"        
+function verificationTypeValue(inputValue, index) {
+
+    if (inputValue === inputValues[0] && inputValues[0].value.match(/^[a-zA-ZÃ\s]+$/) !== null && inputValues[0].value.length > 3) {
+
+        cardRegister["name"] = inputValues[0].value
+
+
+        document.getElementById("name").innerHTML = `${cardRegister["name"]}`
+
+        ErroList[0] = "Corrected"
+
+        ErrMsg(0)
+
+
+        return
     }
-    else 
-    {
-        if (document.getElementById(`input_${2}`)) {
-            inputValues[2].parentNode.removeChild(document.getElementById(`input_${2}`))
+    else if (inputValue.value.match(/^[0-9]+$/) !== null && inputValue.value > 0) {
+
+        verificationValueRecomend(inputValue, index)
+
+        return
+
+    }
+    else {
+
+        if (document.getElementById(`input_${index}`)) {
+
+            inputValues[index].parentNode.removeChild(document.getElementById(`input_${index}`))
+
         }
-        ErroList[2] = "Wrong format, numbers only"
+
+        ErroList[index] = "Wrong format, numbers only"
+
+        ErrMsg(index)
+
+
+        return
     }
-    ErrMsg(2)
-    return
 }
 
-function DayVerification() {
-    
-    if (inputValues[3].value <= daysofMonth[(inputValues[2].value) - 1]) {
+function verificationValueRecomend(inputValue, index) {
+
+    if (inputValue === inputValues[3] && inputValues[3].value.length === 2 && (inputValues[3].value <= daysofMonth[Number(inputValues[2].value) - 1] &&  inputValues[2].value >= 1 ||  inputValues[2].value < 1 && inputValues[3].value <= 31 )) {
+
+        cardRegister["day"] = inputValues[3].value
+
+        document.getElementById("day").innerHTML = `${cardRegister["day"]}`
 
         ErroList[3] = "Corrected"
+
     }
-    else 
-    {
-        if (document.getElementById(`input_${3}`)) {
-            inputValues[3].parentNode.removeChild(document.getElementById(`input_${3}`))
-        }
-        ErroList[3] = "Wrong format, numbers only"
-    }
-    ErrMsg(3)
-    return
-}
+    else if (inputValue === inputValues[2] && inputValues[2].value < 13 && inputValues[2].value.length === 2) {
 
-function VerificationNumberValue(inputValue, index) {
-    if (Number(inputValue.value) || inputValue === inputValues[0]) 
-    {
+        cardRegister["month"] = inputValues[2].value
 
-        if(inputValue === inputValues[2] || inputValue === inputValues[3])
-        {
+        document.getElementById("month").innerHTML = `${cardRegister["month"]}`
 
-            monthVerification()
+        ErroList[2] = "Corrected"
 
-            DayVerification()
+        if (inputValues[3].value > daysofMonth[Number(inputValues[2].value) - 1]) {
             
-            return
-        }
+            ErroList[3] = "Wrong format, numbers only"
 
-        ErroList[index] = "Corrected"
+            ErrMsg(3)
+            
+        }
 
     }
-    else 
-    {
-        if (document.getElementById(`input_${index}`)) {
-            inputValues[index].parentNode.removeChild(document.getElementById(`input_${index}`))
+    else if (inputValue === inputValues[1] && inputValues[1].value.length === 16) {
+
+        cardRegister["cardnumber"] = inputValues[1].value
+
+        for (let i = 0; i < 16; i += 4) {
+
+            if (i === 0) {
+                document.getElementById("cardNumber").innerHTML = `${cardRegister["cardnumber"].slice(i, i + 4)}`
+
+            }
+
+            else {
+
+                document.getElementById("cardNumber").innerHTML += `  ${cardRegister["cardnumber"].slice(i, i + 4)} `
+
+            }
         }
+
+        ErroList[1] = "Corrected"
+
+    }
+    else if (inputValue === inputValues[4] && inputValues[4].value.length === 3) {
+
+        cardRegister["cvc"] = inputValues[4].value
+
+        document.getElementById("cvc").innerHTML = `${cardRegister["cvc"]}`
+
+        ErroList[4] = "Corrected"
+
+    }
+    else {
+
         ErroList[index] = "Wrong format, numbers only"
+
+    }
+
+    if (document.getElementById(`input_${index}`)) {
+
+        inputValues[index].parentNode.removeChild(document.getElementById(`input_${index}`))
+
     }
 
     ErrMsg(index)
+
+
     return
-
 }
-
-
 
 function ErrMsg(i) {
 
-    if (document.getElementById(`input_${i}`) === null) 
-    {
+    if (document.getElementById(`input_${i}`) === null) {
 
         const Errormsg = document.createElement("span")
+
         const ErrorText = document.createTextNode(`${ErroList[i]}`)
+
         Errormsg.appendChild(ErrorText)
+
         Errormsg.setAttribute("class", `Container_ErrorMsg_${i}`)
+
         Errormsg.setAttribute("id", `input_${i}`)
+
         inputValues[i].parentNode.appendChild(Errormsg)
+
 
         inputValues[i].style.border = "1px solid red"
 
     }
 
-    if (document.getElementById(`input_${i}`) && ErroList[i] === "Corrected")
-    {
+    if (document.getElementById(`input_${i}`) && ErroList[i] === "Corrected") {
+
         inputValues[i].parentNode.removeChild(document.getElementById(`input_${i}`))
 
-        inputValues[i].style.border = "1px solid  hsl(270, 3%, 87%)"
+        inputValues[i].style.border = "1px solid hsl(270, 3%, 87%)"
+
     }
+
+
     return
 }
 
